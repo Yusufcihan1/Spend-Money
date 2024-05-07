4ec928a9022b7c212fee4edff414af48792e9702
Yusuf Cihan <50721899+Yusufcihan1@users.noreply.github.com>
2024-05-07 04:23:40 +0300

    Update Data.jsx

--- src/Data.jsx
+++ src/Data.jsx
@@ -3,168 +3,168 @@ export const data = [
         id : 1,
         title : "Ferrari",
         price: 2,
-        image : '/public/img/ferrari.jpg'
+        image : 'https://billgatesmoney.netlify.app/images/big-mac.jpg'
 
     },
     {
         id : 2,
         title : "Flip Flops",
         price: 3,
-        image : '/public/img/flip-flops.jpg'
+        image : 'https://billgatesmoney.netlify.app/images/slipper.jpg'
 
     },
     {
         id : 3,
         title : "Coca-Cola Pack",
         price: 5,
-        image : '/public/img/coca-cola-pack.jpg'
+        image : 'https://billgatesmoney.netlify.app/images/coca-cola.jpg'
 
     },
     {
         id : 4,
         title : "Movie Ticket",
         price: 12,
-        image : '/public/img/movie-ticket.jpg'
+        image : 'https://billgatesmoney.netlify.app/images/movie-ticket.jpg'
 
     },
     {
         id : 5,
         title : "Book",
         price: 15,
-        image : '/public/img/book.jpg'
+        image : 'https://billgatesmoney.netlify.app/images/book.jpg'
 
     },
     {
         id : 6,
         title : "Lobster Dinner",
         price: 45,
-        image : '/public/img/lobster-dinner.jpg'
+        image : 'https://billgatesmoney.netlify.app/images/lobster-dinner.jpg'
 
     },
     {
         id : 7,
         title : "Video Game",
         price: 60,
-        image : '/public/img/video-game.jpg'
+        image : 'https://billgatesmoney.netlify.app/images/video-game.jpg'
 
     },
     {
         id : 8,
         title : "Amazon Echo",
         price: 99,
-        image : '/public/img/amazon-echo.jpg'
+        image : 'https://billgatesmoney.netlify.app/images/amazon-echo.jpg'
 
     },
     {
         id : 9,
         title : "Year Of Netflix",
         price: 100,
-        image : '/public/img/year-of-netflix.jpg'
+        image : 'https://billgatesmoney.netlify.app/images/year-of-netflix.jpg'
 
     },
     {
         id : 10,
         title : "Air Jordans",
-        price: 2,
-        image : '/public/img/air-jordans.jpg'
+        price: 125,
+        image : 'https://billgatesmoney.netlify.app/images/air-jordans.jpg'
 
     },
     {
         id : 11,
         title : "Airpods",
-        price: 2,
-        image : '/public/img/airpods.jpg'
+        price: 199,
+        image : 'https://billgatesmoney.netlify.app/images/airpods.jpg'
 
     },
     {
         id : 12,
         title : "Gaming Console",
-        price: 2,
-        image : '/public/img/gaming-console.jpg'
+        price: 299,
+        image : 'https://billgatesmoney.netlify.app/images/gaming-console.jpg'
 
     },
     {
         id : 13,
         title : "Drone",
-        price: 2,
-        image : '/public/img/drone.jpg'
+        price: 350,
+        image : 'https://billgatesmoney.netlify.app/images/drone.jpg'
 
     },
     {
         id : 14,
         title : "Smartphone",
-        price: 2,
-        image : '/public/img/smartphone.jpg'
+        price: 699,
+        image : 'https://billgatesmoney.netlify.app/images/smartphone.jpg'
 
     },
     {
         id : 15,
         title : "Bike",
-        price: 2,
-        image : '/public/img/bike.jpg'
+        price: 800,
+        image : 'https://billgatesmoney.netlify.app/images/bike.jpg'
 
     },
     {
         id : 16,
         title : "Kitten",
-        price: 2,
-        image : '/public/img/kitten.jpg'
+        price: 1500,
+        image : 'https://billgatesmoney.netlify.app/images/kitten.jpg'
 
     },
     {
         id : 17,
         title : "Puppy",
-        price: 2,
-        image : '/public/img/puppy.jpg'
+        price: 1500,
+        image : 'https://billgatesmoney.netlify.app/images/puppy.jpg'
 
     },
     {
         id : 18,
         title : "Auto Rickshaw",
-        price: 2,
-        image : '/public/img/auto-rickshaw.jpg'
+        price: 2300,
+        image : 'https://billgatesmoney.netlify.app/images/auto-rickshaw.jpg'
 
     },
     {
         id : 19,
         title : "Horse",
-        price: 2,
-        image : '/public/img/horse.jpg'
+        price: 2500,
+        image : 'https://billgatesmoney.netlify.app/images/horse.jpg'
 
     },
     {
         id : 20,
         title : "Acre Of Farmland",
-        price: 2,
-        image : '/public/img/acre-of-farmland.jpg'
+        price: 3000,
+        image : 'https://billgatesmoney.netlify.app/images/acre-of-farmland.jpg'
 
     },
     {
         id : 21,
         title : "Designer Handbag",
-        price: 2,
-        image : '/public/img/designer-handbag.jpg'
+        price: 5500,
+        image : 'https://billgatesmoney.netlify.app/images/designer-handbag.jpg'
 
     },
     {
         id : 22,
         title : "Hot Tub",
-        price: 2,
-        image : '/public/img/hot-tub.jpg'
+        price: 6000,
+        image : 'https://billgatesmoney.netlify.app/images/hot-tub.jpg'
 
     },
     {
         id : 23,
         title : "Luxury Wine",
-        price: 2,
-        image : '/public/img/luxury-wine.jpg'
+        price: 7000,
+        image : 'https://billgatesmoney.netlify.app/images/luxury-wine.jpg'
 
     },
     {
         id : 24,
         title : "Diamond Ring",
-        price: 2,
-        image : '/public/img/diamond-ring.jpg'
+        price: 10000,
+        image : 'https://billgatesmoney.netlify.app/images/diamond-ring.jpg'
 
     }
     
